0ec04227333dfa4cd4d26ca4d3b23d3d9bcf9e1b
wangfeizhou <18819466397@163.com>
2018-05-09 18:06:03 +0800

    修改首页 意见反馈添加类型

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,23 @@
 export default {
-  host: 'http://10.1.2.201:8080',
+  // host: 'http://10.1.2.201:8080',
+  host: 'http://10.1.2.9:8080',
 
   user: '/careerbro/user/user',
 
-  login: '/careerbro/sys/login'
+  login: '/careerbro/sys/login',
+
+  /**
+   * GET /:uid  关注公司
+   * POST  取消关注
+   */
+  attentionCompany: '/careerbro/attention/corperation' ,
+  /**
+   * GET /:uid  关注岗位
+   * POST  取消关注
+   */
+  attentionJob: '/careerbro/attention/industry' ,
+
+  userJoin: '/careerbro/salary/user', // :uid 用户发布薪资信息
+
+  addFeedback: '/careerbro/feedback/feedback', // 发布反馈信息
 }
