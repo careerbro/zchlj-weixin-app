de2b31f4795402589ad6c072df36095834d74f0a
wangfeizhou <18819466397@163.com>
2018-05-24 14:55:12 +0800

    添加登录接口实现

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 export default {
-  // host: 'http://10.1.2.201:8080',
-  host: 'http://10.1.2.9:8080',
+  host: 'http://10.1.2.201:8080',
+  // host: 'http://10.1.2.9:8080',
 
   user: '/careerbro/user/user',
   getUserSum: '/careerbro/user/sum',
@@ -21,4 +21,18 @@ export default {
   userJoin: '/careerbro/salary/user', // :uid 用户发布薪资信息
 
   addFeedback: '/careerbro/feedback/feedback', // 发布反馈信息
+
+  searchCollege: '/careerbro/college/college', // 学校模糊搜索
+  searchDistrict: '/careerbro/district/district', // 地区 模糊搜索
+  searchJob: '/careerbro/job/job', // 岗位 模糊搜索
+  searchIndustry: '/careerbro/industry/industry', // 行业模糊搜索
+  searchCorperation: '/careerbro/corperation/corperation', // 公司模糊搜索
+
+  addSalary: '/careerbro/salary/salary', // post
+
+  comment: '/careerbro/comment/comment', // post
+
+  addSalary1: '/careerbro/salary/salary', // post
+
+  addSalary2: '/careerbro/salary/salary', // post
 }
